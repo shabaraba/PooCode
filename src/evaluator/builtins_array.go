a6be0e96373cd70f5a4ade60c02d6317220b476c
shabaraba <work.shabaraba@gmail.com>
2025-03-27 11:27:30 +0900

    feat(array): map/filter関数の実装

--- src/evaluator/builtins_array.go
+++ src/evaluator/builtins_array.go
@@ -9,7 +9,40 @@ func registerArrayBuiltins() {
 	Builtins["map"] = &object.Builtin{
 		Name: "map",
 		Fn: func(args ...object.Object) object.Object {
-			return NULL
+			if len(args) != 2 {
+				return createError("map function requires 2 arguments")
+			}
+			
+			arr, ok := args[0].(*object.Array)
+			if !ok {
+				return createError("First argument to map must be an array")
+			}
+			
+			fn, ok := args[1].(*object.Function)
+			if !ok {
+				return createError("Second argument to map must be a function")
+			}
+			
+			resultElements := make([]object.Object, 0)
+			
+			for _, elem := range arr.Elements {
+				extendedEnv := object.NewEnclosedEnvironment(fn.Env)
+				extendedEnv.Set("🍕", elem)
+				
+				result := Eval(fn.ASTBody, extendedEnv)
+				
+				if errObj, ok := result.(*object.Error); ok {
+					return errObj
+				}
+				
+				if retVal, ok := result.(*object.ReturnValue); ok {
+					result = retVal.Value
+				}
+				
+				resultElements = append(resultElements, result)
+			}
+			
+			return &object.Array{Elements: resultElements}
 		},
 		ReturnType: object.ARRAY_OBJ,
 		ParamTypes: []object.ObjectType{object.ARRAY_OBJ, object.FUNCTION_OBJ},
@@ -19,7 +52,42 @@ func registerArrayBuiltins() {
 	Builtins["filter"] = &object.Builtin{
 		Name: "filter",
 		Fn: func(args ...object.Object) object.Object {
-			return NULL
+			if len(args) != 2 {
+				return createError("filter function requires 2 arguments")
+			}
+			
+			arr, ok := args[0].(*object.Array)
+			if !ok {
+				return createError("First argument to filter must be an array")
+			}
+			
+			fn, ok := args[1].(*object.Function)
+			if !ok {
+				return createError("Second argument to filter must be a function")
+			}
+			
+			resultElements := make([]object.Object, 0)
+			
+			for _, elem := range arr.Elements {
+				extendedEnv := object.NewEnclosedEnvironment(fn.Env)
+				extendedEnv.Set("🍕", elem)
+				
+				result := Eval(fn.ASTBody, extendedEnv)
+				
+				if errObj, ok := result.(*object.Error); ok {
+					return errObj
+				}
+				
+				if retVal, ok := result.(*object.ReturnValue); ok {
+					result = retVal.Value
+				}
+				
+				if isTruthy(result) {
+					resultElements = append(resultElements, elem)
+				}
+			}
+			
+			return &object.Array{Elements: resultElements}
 		},
 		ReturnType: object.ARRAY_OBJ,
 		ParamTypes: []object.ObjectType{object.ARRAY_OBJ, object.FUNCTION_OBJ},
