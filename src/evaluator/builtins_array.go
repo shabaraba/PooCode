6d243297a6e71bf823d30249ba4934da8bb64aa4
shabaraba <work.shabaraba@gmail.com>
2025-03-27 15:24:32 +0900

    fix: support multiple arguments in map function pipelines

--- src/evaluator/builtins_array.go
+++ src/evaluator/builtins_array.go
@@ -2,38 +2,65 @@ package evaluator
 
 import (
 	"github.com/uncode/object"
+	"github.com/uncode/logger"
 )
 
 func registerArrayBuiltins() {
-		// map function
+	// map function
 	Builtins["map"] = &object.Builtin{
 		Name: "map",
 		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return createError("map function requires 2 arguments")
+			// 最低2つの引数が必要（配列と関数）
+			if len(args) < 2 {
+				return createError("map function requires at least 2 arguments (array and function)")
 			}
 			
+			// 第1引数は配列でないといけない
 			arr, ok := args[0].(*object.Array)
 			if !ok {
 				return createError("First argument to map must be an array")
 			}
 			
-			// Check if the second argument is a function (either user-defined or builtin)
-			var mapFn func(object.Object) object.Object
+			// 第2引数は関数（ユーザー定義関数またはビルトイン関数）
+			var mapFn func([]object.Object) object.Object
+			
+			// 関数の引数（mapへの追加引数があれば保存）
+			extraArgs := args[2:]
 			
 			switch fn := args[1].(type) {
 			case *object.Function:
-				// User-defined function
-				mapFn = func(elem object.Object) object.Object {
+				// ユーザー定義関数
+				mapFn = func(elemArgs []object.Object) object.Object {
 					extendedEnv := object.NewEnclosedEnvironment(fn.Env)
-					extendedEnv.Set("🍕", elem)
 					
+					// 必ず最初の引数を🍕に設定
+					if len(elemArgs) > 0 {
+						extendedEnv.Set("🍕", elemArgs[0])
+					}
+					
+					// 関数が引数を持つ場合、引数を設定
+					if len(fn.Parameters) > 0 && len(elemArgs) > 1 {
+						// 引数の数を確認
+						paramCount := len(fn.Parameters)
+						if len(elemArgs)-1 < paramCount {
+							logger.Debug("関数の引数が少なすぎます: 期待=%d, 実際=%d", paramCount, len(elemArgs)-1)
+						}
+						
+						// 引数をバインド（🍕の次の引数から）
+						for i := 0; i < paramCount && i+1 < len(elemArgs); i++ {
+							extendedEnv.Set(fn.Parameters[i].Value, elemArgs[i+1])
+						}
+					}
+					
+					// 関数本体を評価
 					result := Eval(fn.ASTBody, extendedEnv)
 					
+					// エラー処理
 					if errObj, ok := result.(*object.Error); ok {
 						return errObj
 					}
 					
+					// 戻り値の処理
 					if retVal, ok := result.(*object.ReturnValue); ok {
 						result = retVal.Value
 					}
@@ -41,19 +68,24 @@ func registerArrayBuiltins() {
 					return result
 				}
 			case *object.Builtin:
-				// Builtin function
-				mapFn = func(elem object.Object) object.Object {
-					result := fn.Fn(elem)
-					return result
+				// ビルトイン関数
+				mapFn = func(elemArgs []object.Object) object.Object {
+					return fn.Fn(elemArgs...)
 				}
 			default:
 				return createError("Second argument to map must be a function")
 			}
 			
+			// マップ処理の実行
 			resultElements := make([]object.Object, 0, len(arr.Elements))
 			
 			for _, elem := range arr.Elements {
-				resultElements = append(resultElements, mapFn(elem))
+				// 現在の要素と追加引数を組み合わせる
+				elemArgs := []object.Object{elem}
+				elemArgs = append(elemArgs, extraArgs...)
+				
+				// 各要素に関数を適用
+				resultElements = append(resultElements, mapFn(elemArgs))
 			}
 			
 			return &object.Array{Elements: resultElements}
--- src/evaluator/pipeline_eval.go
+++ src/evaluator/pipeline_eval.go
@@ -63,163 +63,24 @@ func evalPipeline(node *ast.InfixExpression, env *object.Environment) object.Obj
 
 	var result object.Object
 
-	// 右辺の式がCallExpressionの場合、特別に処理
+	// 右辺の式がCallExpressionの場合（関数呼び出し）
 	if callExpr, ok := node.Right.(*ast.CallExpression); ok {
 		logger.Debug("パイプラインの右辺がCallExpressionです")
-
-		// 関数名を取得
-		var funcName string
-		if ident, ok := callExpr.Function.(*ast.Identifier); ok {
-			funcName = ident.Value
-		} else {
-			return createEvalError("パイプラインの右側の関数名を取得できません: %T", callExpr.Function)
-		}
-
-		// 引数を評価（一時環境で評価することで🍕の影響を分離）
-		args := evalExpressions(callExpr.Arguments, tempEnv)
-		if len(args) == 1 && args[0].Type() == object.ERROR_OBJ {
-			return args[0]
-		}
-
-		// デバッグ出力
-		logger.Debug("パイプラインの関数名: %s, 左辺値: %s, 引数: %v\n",
-			funcName, left.Inspect(), args)
-
-		// 引数の配列を作成（第一引数は左辺の値、第二引数以降は関数の引数）
-		allArgs := []object.Object{left}
-		allArgs = append(allArgs, args...)
-
-		// デバッグ: 最終的な引数リストを表示
-		logger.Debug("applyNamedFunction に渡す最終引数リスト: %d 個\n", len(allArgs))
-		for i, arg := range allArgs {
-			logger.Debug("  引数 %d: タイプ=%s, 値=%s\n", i, arg.Type(), arg.Inspect())
-		}
-
-		// 関数を適用（一時環境で🍕を保護）
-		result = applyNamedFunction(tempEnv, funcName, allArgs)
-		logger.Debug("関数 '%s' の適用結果: %s\n", funcName, result.Inspect())
+		return evalPipelineWithCallExpression(left, callExpr, tempEnv)
 	} else {
-		// パイプラインの右側を評価する
-		right := node.Right
-
-		// 右辺が識別子の場合、関数として評価
-		if ident, ok := right.(*ast.Identifier); ok {
+		// 右辺が識別子の場合（関数名のみ）
+		if ident, ok := node.Right.(*ast.Identifier); ok {
 			logger.Debug("識別子としてのパイプライン先: %s\n", ident.Value)
-
 			logger.Debug("パイプラインから applyNamedFunction を呼び出します (関数名: %s)\n", ident.Value)
 
 			// 環境変数 🍕 を設定して名前付き関数呼び出しへ処理を委譲
 			// ここで左辺の値を唯一の引数として渡す
 			args := []object.Object{left}
 
-			// 名前付き関数を適用する（条件付き関数の処理も行う）
-			// 戻り値を変数に格納して、何が返されるか確認する
+			// 名前付き関数を適用する
 			result = applyNamedFunction(tempEnv, ident.Value, args)
 			logger.Debug("パイプライン: 関数 '%s' の実行結果: タイプ=%s, 値=%s\n",
 				ident.Value, result.Type(), result.Inspect())
-		} else if callExpr, ok := right.(*ast.CallExpression); ok {
-			// 右辺が関数呼び出しの場合
-			logger.Debug("関数呼び出しとしてのパイプライン先")
-
-			// 関数名を識別子から直接取得
-			if ident, ok := callExpr.Function.(*ast.Identifier); ok {
-				logger.Debug("パイプラインでビルトイン関数 '%s' を呼び出します\n", ident.Value)
-
-				// 引数を評価（一時環境で評価）
-				args := evalExpressions(callExpr.Arguments, tempEnv)
-
-				// デバッグ: 引数の内容を表示
-				logger.Debug("関数呼び出し '%s' の引数: %d 個\n", ident.Value, len(args))
-				for i, arg := range args {
-					logger.Debug("  引数 %d: %s\n", i, arg.Inspect())
-				}
-				logger.Debug("パイプラインから渡される値: %s\n", left.Inspect())
-
-				// ビルトイン関数を直接取得して呼び出す
-				if builtin, ok := Builtins[ident.Value]; ok {
-					// leftを第一引数、その他の引数は後続
-					allArgs := []object.Object{left}
-					allArgs = append(allArgs, args...)
-
-					logger.Debug("ビルトイン関数 '%s' を実行: 全引数 %d 個\n", ident.Value, len(allArgs))
-					for i, arg := range allArgs {
-						logger.Debug("  引数 %d: %s\n", i, arg.Inspect())
-					}
-
-					result = builtin.Fn(allArgs...)
-					logger.Debug("ビルトイン関数 '%s' の結果: %s\n", ident.Value, result.Inspect())
-				} else {
-					// ビルトイン関数でない場合は名前付き関数として呼び出し
-					allArgs := []object.Object{left}
-
-					// 残りの引数も追加
-					allArgs = append(allArgs, args...)
-
-					// デバッグ: 最終的な引数リストを表示
-					logger.Debug("名前付き関数 '%s' の最終引数リスト: %d 個\n", ident.Value, len(allArgs))
-					for i, arg := range allArgs {
-						logger.Debug("  引数 %d: %s\n", i, arg.Inspect())
-					}
-
-					// 名前付き関数を適用する（条件付き関数の処理も行う）
-					result = applyNamedFunction(tempEnv, ident.Value, allArgs)
-					logger.Debug("パイプライン(callExpr): 関数 '%s' の実行結果: タイプ=%s, 値=%s\n",
-						ident.Value, result.Type(), result.Inspect())
-				}
-			} else {
-				// 識別子以外の関数式を評価
-				function := Eval(callExpr.Function, tempEnv)
-				if function.Type() == object.ERROR_OBJ {
-					return function
-				}
-
-				args := evalExpressions(callExpr.Arguments, tempEnv)
-
-				// 関数オブジェクトの場合、専用の環境変数🍕に左辺の値を設定
-				if fn, ok := function.(*object.Function); ok {
-					extendedEnv := object.NewEnclosedEnvironment(fn.Env)
-
-					// 通常の引数を環境にバインド
-					if len(args) != len(fn.Parameters) {
-						return createEvalError("引数の数が一致しません: 期待=%d, 実際=%d", len(fn.Parameters), len(args))
-					}
-
-					for i, param := range fn.Parameters {
-						extendedEnv.Set(param.Value, args[i])
-					}
-
-					// パイプラインからの値を🍕にセット
-					extendedEnv.Set("🍕", left)
-
-					// 関数本体を評価
-					astBody, ok := fn.ASTBody.(*ast.BlockStatement)
-					if !ok {
-						return createEvalError("関数の本体がBlockStatementではありません")
-					}
-					result = evalBlockStatement(astBody, extendedEnv)
-
-					// 💩値を返す（関数の戻り値）
-					if obj, ok := result.(*object.ReturnValue); ok {
-						result = obj.Value
-						logger.Debug("関数結果(ReturnValue): %s\n", result.Inspect())
-					}
-					logger.Debug("関数結果(直接): %s\n", result.Inspect())
-				} else if builtin, ok := function.(*object.Builtin); ok {
-					// 組み込み関数の場合、leftを第一引数として追加
-					logger.Debug("ビルトイン関数 '%s' を実行\n", callExpr.Function.(*ast.Identifier).Value)
-					args = append([]object.Object{left}, args...)
-
-					logger.Debug("引数: %d個\n", len(args))
-					for i, arg := range args {
-						logger.Debug("  引数 %d: %s\n", i, arg.Inspect())
-					}
-
-					result = builtin.Fn(args...)
-					logger.Debug("ビルトイン関数の結果: %s\n", result.Inspect())
-				} else {
-					return createEvalError("関数ではありません: %s", function.Type())
-				}
-			}
 		} else {
 			// その他の場合は処理できない
 			return createEvalError("パイプラインの右側が関数または識別子ではありません: %T", node.Right)
@@ -232,6 +93,50 @@ func evalPipeline(node *ast.InfixExpression, env *object.Environment) object.Obj
 		env.Set("🍕", originalPizza)
 	}
 
+	return result
+}
+
+// evalPipelineWithCallExpression は関数呼び出しを含むパイプライン処理を評価する
+func evalPipelineWithCallExpression(left object.Object, callExpr *ast.CallExpression, env *object.Environment) object.Object {
+	// 関数名を取得
+	var funcName string
+	if ident, ok := callExpr.Function.(*ast.Identifier); ok {
+		funcName = ident.Value
+	} else {
+		return createEvalError("パイプラインの右側の関数名を取得できません: %T", callExpr.Function)
+	}
+
+	// 引数を評価（一時環境で評価することで🍕の影響を分離）
+	args := evalExpressions(callExpr.Arguments, env)
+	if len(args) > 0 && args[0].Type() == object.ERROR_OBJ {
+		return args[0]
+	}
+
+	// デバッグ出力
+	logger.Debug("パイプラインの関数名: %s, 左辺値: %s, 引数: %v\n",
+		funcName, left.Inspect(), args)
+
+	// 全引数リストを作成（第一引数は左辺の値、第二引数以降は関数呼び出しの引数）
+	allArgs := []object.Object{left}
+	allArgs = append(allArgs, args...)
+
+	// デバッグ: 最終的な引数リストを表示
+	logger.Debug("applyNamedFunction に渡す最終引数リスト: %d 個\n", len(allArgs))
+	for i, arg := range allArgs {
+		logger.Debug("  引数 %d: タイプ=%s, 値=%s\n", i, arg.Type(), arg.Inspect())
+	}
+
+	// 組み込み関数を直接取得して呼び出す
+	if builtin, ok := Builtins[funcName]; ok {
+		logger.Debug("ビルトイン関数 '%s' を実行: 全引数 %d 個\n", funcName, len(allArgs))
+		result := builtin.Fn(allArgs...)
+		logger.Debug("ビルトイン関数 '%s' の結果: %s\n", funcName, result.Inspect())
+		return result
+	}
+
+	// 名前付き関数（ユーザー定義関数）を適用する
+	result := applyNamedFunction(env, funcName, allArgs)
+	logger.Debug("関数 '%s' の適用結果: %s\n", funcName, result.Inspect())
 	return result
 }
 
