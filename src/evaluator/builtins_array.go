137b7ab1c181d9bc6bffa3bb026f7eb0555fbd9e
shabaraba <work.shabaraba@gmail.com>
2025-03-27 10:52:59 +0900

    feat(array): add debug logs to array builtins

--- src/evaluator/builtins_array.go
+++ src/evaluator/builtins_array.go
@@ -1,101 +1,277 @@
 package evaluator
 
 import (
-	"fmt"
-	"strings"
-	"github.com/uncode/object"
+\t\"fmt\"
+\t\"strings\"
+\t\"github.com/uncode/logger\"
+\t\"github.com/uncode/object\"
 )
 
-// registerArrayBuiltins ã¯é…åˆ—é–¢é€£ã®çµ„ã¿è¾¼ã¿é–¢æ•°ã‚’ç™»éŒ²ã™ã‚‹
-func registerArrayBuiltins() {
-	// é…åˆ—ã‚’é€£çµã—ã¦æ–‡å­—åˆ—ã«ã™ã‚‹é–¢æ•°
-	Builtins["join"] = &object.Builtin{
-		Name: "join",
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return createError("joiné–¢æ•°ã¯2ã¤ã®å¼•æ•°ãŒå¿…è¦ã§ã™: %då€‹ä¸ãˆã‚‰ã‚Œã¾ã—ãŸ", len(args))
-			}
-			
-			// ç¬¬1å¼•æ•°ã¯é…åˆ—
-			if args[0].Type() != object.ARRAY_OBJ {
-				return createError("joiné–¢æ•°ã®ç¬¬1å¼•æ•°ã¯é…åˆ—ã§ã‚ã‚‹å¿…è¦ãŒã‚ã‚Šã¾ã™: %s", args[0].Type())
-			}
-			array, _ := args[0].(*object.Array)
-			
-			// ç¬¬2å¼•æ•°ã¯åŒºåˆ‡ã‚Šæ–‡å­—
-			if args[1].Type() != object.STRING_OBJ {
-				return createError("joiné–¢æ•°ã®ç¬¬2å¼•æ•°ã¯æ–‡å­—åˆ—ã§ã‚ã‚‹å¿…è¦ãŒã‚ã‚Šã¾ã™: %s", args[1].Type())
-			}
-			delimiter, _ := args[1].(*object.String)
-			
-			// é…åˆ—ã®å„è¦ç´ ã‚’æ–‡å­—åˆ—ã«å¤‰æ›
-			elements := make([]string, len(array.Elements))
-			for i, elem := range array.Elements {
-				switch e := elem.(type) {
-				case *object.String:
-					elements[i] = e.Value
-				case *object.Integer:
-					elements[i] = fmt.Sprintf("%d", e.Value)
-				case *object.Boolean:
-					elements[i] = fmt.Sprintf("%t", e.Value)
-				default:
-					elements[i] = e.Inspect()
-				}
-			}
-			
-			return &object.String{Value: strings.Join(elements, delimiter.Value)}
-		},
-		ReturnType: object.STRING_OBJ,
-		ParamTypes: []object.ObjectType{object.ARRAY_OBJ, object.STRING_OBJ},
-	}
+// log variables for debugging
+var builtinLogLevel = logger.LevelTrace
 
-	// æ•°å€¤ã‚·ãƒ¼ã‚±ãƒ³ã‚¹ã‚’ä½œæˆã™ã‚‹é–¢æ•°
-	Builtins["range"] = &object.Builtin{
-		Name: "range",
-		Fn: func(args ...object.Object) object.Object {
-			// å¼•æ•°ã®æ•°ã‚’ãƒã‚§ãƒƒã‚¯: 1ã¾ãŸã¯2ã¤ã®å¼•æ•°ã‚’å—ã‘ä»˜ã‘ã‚‹
-			if len(args) < 1 || len(args) > 2 {
-				return createError("rangeé–¢æ•°ã¯1-2å€‹ã®å¼•æ•°ãŒå¿…è¦ã§ã™: %då€‹ä¸ãˆã‚‰ã‚Œã¾ã—ãŸ", len(args))
-			}
-			
-			var start, end int64
-			
-			// 1ã¤ã®å¼•æ•°ã®å ´åˆ: 0ã‹ã‚‰ãã®å€¤ã¾ã§
-			if len(args) == 1 {
-				if args[0].Type() != object.INTEGER_OBJ {
-					return createError("rangeé–¢æ•°ã®å¼•æ•°ã¯æ•´æ•°ã§ã‚ã‚‹å¿…è¦ãŒã‚ã‚Šã¾ã™: %s", args[0].Type())
-				}
-				endVal, _ := args[0].(*object.Integer)
-				
-				start = 0
-				end = endVal.Value
-			} else {
-				// 2ã¤ã®å¼•æ•°ã®å ´åˆ: startã‹ã‚‰endã¾ã§
-				if args[0].Type() != object.INTEGER_OBJ || args[1].Type() != object.INTEGER_OBJ {
-					return createError("rangeé–¢æ•°ã®å¼•æ•°ã¯æ•´æ•°ã§ã‚ã‚‹å¿…è¦ãŒã‚ã‚Šã¾ã™")
-				}
-				
-				startVal, _ := args[0].(*object.Integer)
-				endVal, _ := args[1].(*object.Integer)
-				
-				start = startVal.Value
-				end = endVal.Value
-			}
-			
-			// é–‹å§‹ä½ç½®ãŒçµ‚äº†ä½ç½®ã‚ˆã‚Šå¤§ãã„å ´åˆã¯ç©ºã®é…åˆ—ã‚’è¿”ã™
-			if start > end {
-				return &object.Array{Elements: []object.Object{}}
-			}
-			
-			// é…åˆ—ã‚’ä½œæˆ
-			elements := make([]object.Object, end-start)
-			for i := start; i < end; i++ {
-				elements[i-start] = &object.Integer{Value: i}
-			}
-			
-			return &object.Array{Elements: elements}
-		},
-		ReturnType: object.ARRAY_OBJ,
-		ParamTypes: []object.ObjectType{object.INTEGER_OBJ, object.INTEGER_OBJ},
-	}
+// logIfEnabled logs a message if logging is enabled for the given level
+func logIfEnabled(level logger.LogLevel, format string, args ...interface{}) {
+\tif logger.IsSpecialLevelEnabled(level) || logger.GetComponentLevel(logger.ComponentBuiltin) >= level {
+\t\tlogger.ComponentDebug(logger.ComponentBuiltin, format, args...)
+\t}
+}
+
+// registerArrayBuiltins oM¢#nD¼¢p’{2Y‹
+func registerArrayBuiltins() {
+\t// M’#PWf‡WkY‹¢p
+\tBuiltins[\"join\"] = &object.Builtin{
+\t\tName: \"join\",
+\t\tFn: func(args ...object.Object) object.Object {
+\t\t\tlogIfEnabled(builtinLogLevel, \"join¢pL|súUŒ~W_: pp=%d\", len(args))
+\t\t\t
+\t\t\tif len(args) != 2 {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"join¢po2dnpLÅgY: %dH‰Œ~W_\", len(args))
+\t\t\t\treturn createError(\"join¢po2dnpLÅgY: %dH‰Œ~W_\", len(args))
+\t\t\t}
+\t\t\t
+\t\t\t// ,1poM
+\t\t\tif args[0].Type() != object.ARRAY_OBJ {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"join¢pn,1poMgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t\treturn createError(\"join¢pn,1poMgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t}
+\t\t\tarray, _ := args[0].(*object.Array)
+\t\t\t
+\t\t\t// ,2po:Š‡W
+\t\t\tif args[1].Type() != object.STRING_OBJ {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"join¢pn,2po‡WgB‹ÅLBŠ~Y: %s\", args[1].Type())
+\t\t\t\treturn createError(\"join¢pn,2po‡WgB‹ÅLBŠ~Y: %s\", args[1].Type())
+\t\t\t}
+\t\t\tdelimiter, _ := args[1].(*object.String)
+\t\t\t
+\t\t\tlogIfEnabled(builtinLogLevel, \"join¢p: M p=%d, :Š‡W='%s'\", len(array.Elements), delimiter.Value)
+\t\t\t
+\t\t\t// Mn ’‡Wk	Û
+\t\t\telements := make([]string, len(array.Elements))
+\t\t\tfor i, elem := range array.Elements {
+\t\t\t\tswitch e := elem.(type) {
+\t\t\t\tcase *object.String:
+\t\t\t\t\telements[i] = e.Value
+\t\t\t\tcase *object.Integer:
+\t\t\t\t\telements[i] = fmt.Sprintf(\"%d\", e.Value)
+\t\t\t\tcase *object.Boolean:
+\t\t\t\t\telements[i] = fmt.Sprintf(\"%t\", e.Value)
+\t\t\t\tdefault:
+\t\t\t\t\telements[i] = e.Inspect()
+\t\t\t\t}
+\t\t\t}
+\t\t\t
+\t\t\tresult := strings.Join(elements, delimiter.Value)
+\t\t\tlogIfEnabled(builtinLogLevel, \"join¢p: Pœ='%s'\", result)
+\t\t\treturn &object.String{Value: result}
+\t\t},
+\t\tReturnType: object.STRING_OBJ,
+\t\tParamTypes: []object.ObjectType{object.ARRAY_OBJ, object.STRING_OBJ},
+\t}
+
+\t// p$·ü±ó¹’\Y‹¢p
+\tBuiltins[\"range\"] = &object.Builtin{
+\t\tName: \"range\",
+\t\tFn: func(args ...object.Object) object.Object {
+\t\t\tlogIfEnabled(builtinLogLevel, \"range¢pL|súUŒ~W_: pp=%d\", len(args))
+\t\t\t
+\t\t\t// pnp’Á§Ã¯: 1~_o2dnp’×QØQ‹
+\t\t\tif len(args) < 1 || len(args) > 2 {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"range¢po1-2npLÅgY: %dH‰Œ~W_\", len(args))
+\t\t\t\treturn createError(\"range¢po1-2npLÅgY: %dH‰Œ~W_\", len(args))
+\t\t\t}
+\t\t\t
+\t\t\tvar start, end int64
+\t\t\t
+\t\t\t// 1dnpn4: 0K‰]n$~g
+\t\t\tif len(args) == 1 {
+\t\t\t\tif args[0].Type() != object.INTEGER_OBJ {
+\t\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"range¢pnpotpgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t\t\treturn createError(\"range¢pnpotpgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t\t}
+\t\t\t\tendVal, _ := args[0].(*object.Integer)
+\t\t\t\t
+\t\t\t\tstart = 0
+\t\t\t\tend = endVal.Value
+\t\t\t\tlogIfEnabled(builtinLogLevel, \"range¢p: 0K‰%d~gnÄò’\", end)
+\t\t\t} else {
+\t\t\t\t// 2dnpn4: startK‰end~g
+\t\t\t\tif args[0].Type() != object.INTEGER_OBJ || args[1].Type() != object.INTEGER_OBJ {
+\t\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"range¢pnpotpgB‹ÅLBŠ~Y\")
+\t\t\t\t\treturn createError(\"range¢pnpotpgB‹ÅLBŠ~Y\")
+\t\t\t\t}
+\t\t\t\t
+\t\t\t\tstartVal, _ := args[0].(*object.Integer)
+\t\t\t\tendVal, _ := args[1].(*object.Integer)
+\t\t\t\t
+\t\t\t\tstart = startVal.Value
+\t\t\t\tend = endVal.Value
+\t\t\t\tlogIfEnabled(builtinLogLevel, \"range¢p: %dK‰%d~gnÄò’\", start, end)
+\t\t\t}
+\t\t\t
+\t\t\t// ‹ËMnLB†MnˆŠ'MD4oznM’ÔY
+\t\t\tif start > end {
+\t\t\t\tlogger.ComponentWarn(logger.ComponentBuiltin, \"range¢p: ‹Ë$ %d LB†$ %d ˆŠ'MD_zM’ÔW~Y\", start, end)
+\t\t\t\treturn &object.Array{Elements: []object.Object{}}
+\t\t\t}
+\t\t\t
+\t\t\t// M’\
+\t\t\telements := make([]object.Object, end-start)
+\t\t\tfor i := start; i < end; i++ {
+\t\t\t\telements[i-start] = &object.Integer{Value: i}
+\t\t\t}
+\t\t\t
+\t\t\tlogIfEnabled(builtinLogLevel, \"range¢p: PœnM p=%d\", len(elements))
+\t\t\treturn &object.Array{Elements: elements}
+\t\t},
+\t\tReturnType: object.ARRAY_OBJ,
+\t\tParamTypes: []object.ObjectType{object.INTEGER_OBJ, object.INTEGER_OBJ},
+\t}
+\t
+\t// map¢p - Mn k¢p’i(Y‹
+\tBuiltins[\"map\"] = &object.Builtin{
+\t\tName: \"map\",
+\t\tFn: func(args ...object.Object) object.Object {
+\t\t\tlogIfEnabled(builtinLogLevel, \"map¢pL|súUŒ~W_: pp=%d\", len(args))
+\t\t\t
+\t\t\t// pnpÁ§Ã¯
+\t\t\tif len(args) != 2 {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"map¢po2dnpLÅgY: M, ¢p\")
+\t\t\t\treturn createError(\"map¢po2dnpLÅgY: M, ¢p\")
+\t\t\t}
+\t\t\t
+\t\t\t// ,1pLMKÁ§Ã¯
+\t\t\tarr, ok := args[0].(*object.Array)
+\t\t\tif !ok {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"map¢pn,1poMgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t\treturn createError(\"map¢pn,1poMgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t}
+\t\t\t
+\t\t\t// ,2pL¢pKÁ§Ã¯
+\t\t\tfn, ok := args[1].(*object.Function)
+\t\t\tif !ok {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"map¢pn,2po¢pgB‹ÅLBŠ~Y: %s\", args[1].Type())
+\t\t\t\treturn createError(\"map¢pn,2po¢pgB‹ÅLBŠ~Y: %s\", args[1].Type())
+\t\t\t}
+\t\t\t
+\t\t\t// map¢pnpnÑéáü¿ozgB‹ÅLB‹
+\t\t\tif len(fn.Parameters) > 0 {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"map¢pk!UŒ_¢poÑéáü¿ü’Ö‹yMgoBŠ~[“\")
+\t\t\t\treturn createError(\"map¢pk!UŒ_¢poÑéáü¿ü’Ö‹yMgoBŠ~[“\")
+\t\t\t}
+\t\t\t
+\t\t\tlogIfEnabled(builtinLogLevel, \"map¢p: M p=%d\", len(arr.Elements))
+\t\t\t
+\t\t\t// PœnM
+\t\t\tresultElements := make([]object.Object, 0, len(arr.Elements))
+\t\t\t
+\t\t\t// Mn k¢p’i(
+\t\t\tfor i, elem := range arr.Elements {
+\t\t\t\tlogIfEnabled(builtinLogLevel, \"map¢p:   %d ’æ-: %s\", i, elem.Inspect())
+\t\t\t\t
+\t\t\t\t// ¢pn°ƒ’á5Wf<Ukş(n ’-š
+\t\t\t\textendedEnv := object.NewEnclosedEnvironment(fn.Env)
+\t\t\t\textendedEnv.Set(\"<U\", elem)
+\t\t\t\t
+\t\t\t\t// ¢p’U¡
+\t\t\t\tresult := Eval(fn.ASTBody, extendedEnv)
+\t\t\t\t
+\t\t\t\t// ¨éüÁ§Ã¯
+\t\t\t\tif errObj, ok := result.(*object.Error); ok {
+\t\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"map¢pnæ-k¨éüLz: %s\", errObj.Message)
+\t\t\t\t\treturn errObj
+\t\t\t\t}
+\t\t\t\t
+\t\t\t\t// ReturnValue’¢óéÃ×
+\t\t\t\tif retVal, ok := result.(*object.ReturnValue); ok {
+\t\t\t\t\tresult = retVal.Value
+\t\t\t\t}
+\t\t\t\t
+\t\t\t\tlogIfEnabled(builtinLogLevel, \"map¢p:   %d næPœ: %s\", i, result.Inspect())
+\t\t\t\t
+\t\t\t\t// Pœ’Mkı 
+\t\t\t\tresultElements = append(resultElements, result)
+\t\t\t}
+\t\t\t
+\t\t\tlogIfEnabled(builtinLogLevel, \"map¢p: æŒ†, PœnM p=%d\", len(resultElements))
+\t\t\treturn &object.Array{Elements: resultElements}
+\t\t},
+\t\tReturnType: object.ARRAY_OBJ,
+\t\tParamTypes: []object.ObjectType{object.ARRAY_OBJ, object.FUNCTION_OBJ},
+\t}
+\t
+\t// filter¢p - aökôY‹ n’½úY‹
+\tBuiltins[\"filter\"] = &object.Builtin{
+\t\tName: \"filter\",
+\t\tFn: func(args ...object.Object) object.Object {
+\t\t\tlogIfEnabled(builtinLogLevel, \"filter¢pL|súUŒ~W_: pp=%d\", len(args))
+\t\t\t
+\t\t\t// pnpÁ§Ã¯
+\t\t\tif len(args) != 2 {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"filter¢po2dnpLÅgY: M, ¢p\")
+\t\t\t\treturn createError(\"filter¢po2dnpLÅgY: M, ¢p\")
+\t\t\t}
+\t\t\t
+\t\t\t// ,1pLMKÁ§Ã¯
+\t\t\tarr, ok := args[0].(*object.Array)
+\t\t\tif !ok {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"filter¢pn,1poMgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t\treturn createError(\"filter¢pn,1poMgB‹ÅLBŠ~Y: %s\", args[0].Type())
+\t\t\t}
+\t\t\t
+\t\t\t// ,2pL¢pKÁ§Ã¯
+\t\t\tfn, ok := args[1].(*object.Function)
+\t\t\tif !ok {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"filter¢pn,2po¢pgB‹ÅLBŠ~Y: %s\", args[1].Type())
+\t\t\t\treturn createError(\"filter¢pn,2po¢pgB‹ÅLBŠ~Y: %s\", args[1].Type())
+\t\t\t}
+\t\t\t
+\t\t\t// filter¢pnpnÑéáü¿ozgB‹ÅLB‹
+\t\t\tif len(fn.Parameters) > 0 {
+\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"filter¢pk!UŒ_¢poÑéáü¿ü’Ö‹yMgoBŠ~[“\")
+\t\t\t\treturn createError(\"filter¢pk!UŒ_¢poÑéáü¿ü’Ö‹yMgoBŠ~[“\")
+\t\t\t}
+\t\t\t
+\t\t\tlogIfEnabled(builtinLogLevel, \"filter¢p: M p=%d\", len(arr.Elements))
+\t\t\t
+\t\t\t// PœnM
+\t\t\tresultElements := make([]object.Object, 0)
+\t\t\t
+\t\t\t// Mn kaö¢p’i(
+\t\t\tfor i, elem := range arr.Elements {
+\t\t\t\tlogIfEnabled(builtinLogLevel, \"filter¢p:   %d ’æ-: %s\", i, elem.Inspect())
+\t\t\t\t
+\t\t\t\t// ¢pn°ƒ’á5Wf<Ukş(n ’-š
+\t\t\t\textendedEnv := object.NewEnclosedEnvironment(fn.Env)
+\t\t\t\textendedEnv.Set(\"<U\", elem)
+\t\t\t\t
+\t\t\t\t// aö¢p’U¡
+\t\t\t\tresult := Eval(fn.ASTBody, extendedEnv)
+\t\t\t\t
+\t\t\t\t// ¨éüÁ§Ã¯
+\t\t\t\tif errObj, ok := result.(*object.Error); ok {
+\t\t\t\t\tlogger.ComponentError(logger.ComponentBuiltin, \"filter¢pnæ-k¨éüLz: %s\", errObj.Message)
+\t\t\t\t\treturn errObj
+\t\t\t\t}
+\t\t\t\t
+\t\t\t\t// ReturnValue’¢óéÃ×
+\t\t\t\tif retVal, ok := result.(*object.ReturnValue); ok {
+\t\t\t\t\tresult = retVal.Value
+\t\t\t\t}
+\t\t\t\t
+\t\t\t\t// PœLn4 ’PœMkı 
+\t\t\t\tif isTruthy(result) {
+\t\t\t\t\tlogIfEnabled(builtinLogLevel, \"filter¢p:   %d oaö’€_W~Y\", i)
+\t\t\t\t\tresultElements = append(resultElements, elem)
+\t\t\t\t} else {
+\t\t\t\t\tlogIfEnabled(builtinLogLevel, \"filter¢p:   %d oaö’€_W~[“\", i)
+\t\t\t\t}
+\t\t\t}
+\t\t\t
+\t\t\tlogIfEnabled(builtinLogLevel, \"filter¢p: æŒ†, PœnM p=%d\", len(resultElements))
+\t\t\treturn &object.Array{Elements: resultElements}
+\t\t},
+\t\tReturnType: object.ARRAY_OBJ,
+\t\tParamTypes: []object.ObjectType{object.ARRAY_OBJ, object.FUNCTION_OBJ},
+\t}
 }
