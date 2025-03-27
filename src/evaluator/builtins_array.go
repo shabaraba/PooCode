3c8e6f10b7ae2cb2edd756124e853d91d620e416
shabaraba <work.shabaraba@gmail.com>
2025-03-27 11:06:56 +0900

    refactor(array): simplify array builtins implementation

--- src/evaluator/builtins_array.go
+++ src/evaluator/builtins_array.go
@@ -7,40 +7,97 @@ import (
 	"github.com/uncode/object"
 )
 
-// log variables for debugging
-var builtinLogLevel = logger.LevelTrace
-
-// logIfEnabled logs a message if logging is enabled for the given level
-func logIfEnabled(level logger.LogLevel, format string, args ...interface{}) {
-	if logger.IsSpecialLevelEnabled(level) || logger.GetComponentLevel(logger.ComponentBuiltin) >= level {
-		logger.ComponentDebug(logger.ComponentBuiltin, format, args...)
-	}
-}
-
 // registerArrayBuiltins は配列関連の組み込み関数を登録する
 func registerArrayBuiltins() {
-	// 配列を連結して文字列にする関数
-	Builtins["join"] = &object.Builtin{
-		Name: "join",
+	// map関数 - 配列の各要素に関数を適用する
+	Builtins["map"] = &object.Builtin{
+		Name: "map",
 		Fn: func(args ...object.Object) object.Object {
-			logIfEnabled(builtinLogLevel, "join関数が呼び出されました: 引数数=%d", len(args))
-			
-			if len(args) \!= 2 {
-				logger.ComponentError(logger.ComponentBuiltin, "join関数は2つの引数が必要です: %d個与えられました", len(args))
-				return createError("join関数は2つの引数が必要です: %d個与えられました", len(args))
+			// 引数の数チェック
+			if len(args) != 2 {
+				return createError("map関数は2つの引数が必要です: 配列, 関数")
 			}
+			
+			// 第1引数が配列かチェック
+			arr, ok := args[0].(*object.Array)
+			if !ok {
+				return createError("map関数の第1引数は配列である必要があります: %s", args[0].Type())
+			}
+			
+			// 第2引数が関数かチェック
+			fn, ok := args[1].(*object.Function)
+			if !ok {
+				return createError("map関数の第2引数は関数である必要があります: %s", args[1].Type())
+			}
+			
+			// map関数の引数のパラメータは空である必要がある
+			if len(fn.Parameters) > 0 {
+				return createError("map関数に渡された関数はパラメーターを取るべきではありません")
+			}
+			
+			// 結果の配列
+			resultElements := make([]object.Object, 0, len(arr.Elements))
+			
+			// 配列の各要素に関数を適用
+			for _, elem := range arr.Elements {
+				// 関数の環境を拡張して🍕に現在の要素を設定
+				extendedEnv := object.NewEnclosedEnvironment(fn.Env)
+				extendedEnv.Set("🍕", elem)
+				
+				// 関数を評価
+				result := Eval(fn.ASTBody, extendedEnv)
+				
+				// エラーチェック
+				if errObj, ok := result.(*object.Error); ok {
+					return errObj
+				}
+				
+				// ReturnValueをアンラップ
+				if retVal, ok := result.(*object.ReturnValue); ok {
+					result = retVal.Value
+				}
+				
+				// 結果を配列に追加
+				resultElements = append(resultElements, result)
+			}
+			
+			return &object.Array{Elements: resultElements}
+		},
+		ReturnType: object.ARRAY_OBJ,
+		ParamTypes: []object.ObjectType{object.ARRAY_OBJ, object.FUNCTION_OBJ},
+	}
+	
+	// filter関数 - 条件に合致する要素のみを抽出する
+	Builtins["filter"] = &object.Builtin{
+		Name: "filter",
+		Fn: func(args ...object.Object) object.Object {
+			// 引数の数チェック
+			if len(args) != 2 {
+				return createError("filter関数は2つの引数が必要です: 配列, 関数")
+			}
+			
+			// 第1引数が配列かチェック
+			arr, ok := args[0].(*object.Array)
+			if !ok {
+				return createError("filter関数の第1引数は配列である必要があります: %s", args[0].Type())
+			}
+			
+			// 第2引数が関数かチェック
+			fn, ok := args[1].(*object.Function)
+			if !ok {
+				return createError("filter関数の第2引数は関数である必要があります: %s", args[1].Type())
+			}
+			
+			// filter関数の引数のパラメータは空である必要がある
+			if len(fn.Parameters) > 0 {
 				return createError("filter関数に渡された関数はパラメーターを取るべきではありません")
 			}
-			
-			logIfEnabled(builtinLogLevel, "filter関数: 配列要素数=%d", len(arr.Elements))
 			
 			// 結果の配列
 			resultElements := make([]object.Object, 0)
 			
 			// 配列の各要素に条件関数を適用
-			for i, elem := range arr.Elements {
-				logIfEnabled(builtinLogLevel, "filter関数: 要素 %d を処理中: %s", i, elem.Inspect())
-				
+			for _, elem := range arr.Elements {
 				// 関数の環境を拡張して🍕に現在の要素を設定
 				extendedEnv := object.NewEnclosedEnvironment(fn.Env)
 				extendedEnv.Set("🍕", elem)
@@ -50,7 +107,6 @@ func registerArrayBuiltins() {
 				
 				// エラーチェック
 				if errObj, ok := result.(*object.Error); ok {
-					logger.ComponentError(logger.ComponentBuiltin, "filter関数の処理中にエラーが発生: %s", errObj.Message)
 					return errObj
 				}
 				
@@ -61,14 +117,10 @@ func registerArrayBuiltins() {
 				
 				// 結果が真の場合、要素を結果配列に追加
 				if isTruthy(result) {
-					logIfEnabled(builtinLogLevel, "filter関数: 要素 %d は条件を満たします", i)
 					resultElements = append(resultElements, elem)
-				} else {
-					logIfEnabled(builtinLogLevel, "filter関数: 要素 %d は条件を満たしません", i)
 				}
 			}
 			
-			logIfEnabled(builtinLogLevel, "filter関数: 処理完了, 結果の配列要素数=%d", len(resultElements))
 			return &object.Array{Elements: resultElements}
 		},
 		ReturnType: object.ARRAY_OBJ,
