08ee13a00e57116d88ea1f9276b8e0be26fb379f
shabaraba <work.shabaraba@gmail.com>
2025-03-28 11:54:44 +0900

    feat: split runtime executor into modular parts for better maintainability

---
+++ src/runtime/executor_part2.go
@@ -0,0 +1,33 @@
+// SetupBuiltins は組み込み関数を環境に設定する
+func SetupBuiltins(env *object.Environment) {
+	// プリント関数を追加
+	env.Set("print", &object.Builtin{
+		Name: "print",
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return evaluator.NullObj
+		},
+	})
+	
+	// 評価器から組み込み関数をすべてインポート
+	// evaluator.Builtinsに登録されている関数をすべて環境に追加
+	for name, builtin := range evaluator.Builtins {
+		logger.Debug("組み込み関数を登録: %s", name)
+		env.Set(name, builtin)
+	}
+}
+
+// convertToObjectIdentifiers は ast.Identifier スライスを object.Identifier スライスに変換する
+func convertToObjectIdentifiers(params []*ast.Identifier) []*object.Identifier {
+	if params == nil {
+		return nil
+	}
+	
+	result := make([]*object.Identifier, len(params))
+	for i, param := range params {
+		result[i] = &object.Identifier{Value: param.Value}
+	}
+	return result
+}
---
+++ src/runtime/executor_part3.go
@@ -0,0 +1,89 @@
+// preRegisterFunctions は ASTをトラバースして関数定義を事前に環境に登録する
+func preRegisterFunctions(program *ast.Program, env *object.Environment) {
+	if program == nil || len(program.Statements) == 0 {
+		return
+	}
+
+	logger.Debug("関数の事前登録を開始します...")
+	registeredCount := 0
+
+	// プログラム内の全てのステートメントを走査
+	for _, stmt := range program.Statements {
+		// ExpressionStatement 内の FunctionLiteral を検出
+		// FUNCTION (def) で始まる式文を見つける
+		if exprStmt, ok := stmt.(*ast.ExpressionStatement); ok {
+			// 関数リテラルの場合
+			if funcLit, ok := exprStmt.Expression.(*ast.FunctionLiteral); ok {
+				if funcLit.Name != nil {
+					// 関数を環境に登録
+					function := &object.Function{
+						Parameters: convertToObjectIdentifiers(funcLit.Parameters),
+						ASTBody:    funcLit.Body,
+						Env:        env,
+						InputType:  funcLit.InputType,
+						ReturnType: funcLit.ReturnType,
+						Condition:  funcLit.Condition,
+					}
+
+					// 関数名を取得
+					funcName := funcLit.Name.Value
+					logger.Debug("FunctionLiteral: 関数 '%s' の定義を見つけました", funcName)
+
+					// 条件付き関数の場合の特別な処理
+					if funcLit.Condition != nil {
+						// 既存の同名関数の数をカウント
+						existingFuncs := env.GetAllFunctionsByName(funcName)
+						uniqueName := fmt.Sprintf("%s#%d", funcName, len(existingFuncs))
+
+						logger.Debug("条件付き関数 '%s' を '%s' として事前登録します", funcName, uniqueName)
+
+						// 特別な名前で登録
+						env.Set(uniqueName, function)
+					}
+
+					// 通常の名前でも登録
+					env.Set(funcName, function)
+					registeredCount++
+					logger.Debug("関数 '%s' を事前登録しました", funcName)
+				}
+			}
+		}
+
+		// AssignStatement 内の FunctionLiteral を検出（関数を変数に代入するケース）
+		if assignStmt, ok := stmt.(*ast.AssignStatement); ok {
+			if funcLit, ok := assignStmt.Value.(*ast.FunctionLiteral); ok {
+				if ident, ok := assignStmt.Left.(*ast.Identifier); ok {
+					logger.Debug("AssignStatement: 関数を変数 '%s' に代入する定義を見つけました", ident.Value)
+					
+					// 関数を環境に登録
+					function := &object.Function{
+						Parameters: convertToObjectIdentifiers(funcLit.Parameters),
+						ASTBody:    funcLit.Body,
+						Env:        env,
+						InputType:  funcLit.InputType,
+						ReturnType: funcLit.ReturnType,
+						Condition:  funcLit.Condition,
+					}
+
+					// 代入先の変数名を関数名として使用
+					funcName := ident.Value
+					env.Set(funcName, function)
+					registeredCount++
+					logger.Debug("代入式の関数 '%s' を事前登録しました", funcName)
+				}
+			}
+		}
+	}
+
+	// 第二パス: すべてのステートメントを再度走査して、埋もれた関数定義を見つける
+	// 特に、コメントの後に出現する可能性がある関数定義を見つけるため
+	for _, stmt := range program.Statements {
+		// トップレベルの式を探索
+		if exprStmt, ok := stmt.(*ast.ExpressionStatement); ok {
+			// より複雑な式の中にある関数定義を掘り下げる
+			findAndRegisterFunctionsInExpression(exprStmt.Expression, env, &registeredCount)
+		}
+	}
+
+	logger.Debug("関数の事前登録が完了しました。%d 個の関数を登録しました", registeredCount)
+}
